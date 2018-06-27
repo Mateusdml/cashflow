616d96700f5606ec2035a9f4cf9f55eab97b78ef
anadunning <anakpedrosa@hotmail.com>
2018-06-26 23:03:02 -0300

    Implementada a classe MovimentacaoDAO, modificados os nomes dos pacotes para o padrao.

---
+++ src/main/java/br/edu/ifpb/pweb2/cashflow/controller/Resultado.java
@@ -0,0 +1,44 @@
+package br.edu.ifpb.pweb2.cashflow.controller;
+
+import br.edu.ifpb.pweb2.cashflow.model.Usuario;
+
+public class Resultado {
+
+	public boolean isErro() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public String getProximaPagina() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean isRedirect() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public void setProximaPagina(String paginaSucesso) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setRedirect(boolean b) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Usuario getModel() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object getMensagens() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+
+}
